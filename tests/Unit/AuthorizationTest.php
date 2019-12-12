a18f1111122c3fb83d882357f70b5f23c0f371fc
gulverd <vakhtang.pataraia@gmail.com>
2019-12-12 16:19:22 +0400

    authorization-tests

---
+++ tests/Unit/AuthorizationTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class AuthorizationTest extends TestCase
+{
+	public $user;
+    public function setUp():void
+    {
+    	parent::setUp();
+
+    	$this -> user = factory(User::class)->create();
+    }
+
+    public function testLogin()
+    {
+    	$user 		  = $this -> user; 
+    	$request 	  = [
+    		'phone_number' => $user -> phone_number,
+    		'password' 	   => 'password'
+    	];
+    	$responseJson = $this -> post('/api/app/V1/login', $request);
+    	$responseJson -> assertStatus(200);
+    }
+
+   	public function testLoginFailed()
+   	{
+   		$user 		  = $this -> user; 
+    	$request 	  = [
+    		'phone_number' => $user -> phone_number,
+    		'password' 	   => '12'
+    	];
+    	$responseJson = $this -> post('/api/app/V1/login', $request);
+		$responseJson -> assertStatus(401);   	
+	}
+	public function testLoginToken()
+	{
+		$user 		  = $this -> user; 
+    	$request 	  = [
+    		'phone_number' => $user -> phone_number,
+    		'password' 	   => 'password'
+    	];
+    	$responseJson = $this -> post('/api/app/V1/login', $request);
+    	$this -> assertEquals('bearer', $responseJson -> original['token_type']);
+	}
+}
