707f87862278999e71b6350f17f2c4c43f17e622
gulverd <vakhtang.pataraia@gmail.com>
2019-12-23 13:03:09 +0400

    create_chargers_api_resources

--- app/Http/Controllers/Api/app/V1/ChargerController.php
+++ app/Http/Controllers/Api/app/V1/ChargerController.php
@@ -3,23 +3,39 @@ namespace App\Http\Controllers\Api\app\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Charger;
+use App\Http\Resources\ChargerCollection;
 use App\Library\Charger as ChargerLibrary;
+use App\Http\Resources\Charger as ChargerResource;
 
 class ChargerController extends Controller
 {
     public function getChargers(ChargerLibrary $chargerLibrary)
     {
-    	$chargers = Charger::OrderBy('id', 'desc') -> get();
-        return response() -> json([
-            'charger' => $chargerLibrary -> getChargerArray($chargers)
-        ], 200);
+        return new ChargerCollection(Charger::OrderBy('id', 'desc') ->with([
+            'tags' , 
+            'connector_types', 
+            'charger_types',
+            'charging_prices',
+            'fast_charging_prices'
+            ]) -> get());
+    	// $chargers = Charger::OrderBy('id', 'desc') -> get();
+     //    return response() -> json([
+     //        'charger' => $chargerLibrary -> getChargerArray($chargers)
+     //    ], 200);
     }
 
     public function getSingleCharger(ChargerLibrary $chargerLibrary, $charger_id)
     {
-    	$chargers = Charger::where('id', $charger_id) -> get();
-        return response() -> json([
-            'charger' => $chargerLibrary -> getChargerArray($chargers)
-        ], 200);
+        return new ChargerResource(Charger::where('id',$charger_id)->with([
+            'tags' , 
+            'connector_types', 
+            'charger_types',
+            'charging_prices',
+            'fast_charging_prices'
+            ]) -> first());
+    	// $chargers = Charger::where('id', $charger_id) -> get();
+     //    return response() -> json([
+     //        'charger' => $chargerLibrary -> getChargerArray($chargers)
+     //    ], 200);
     }
 }
