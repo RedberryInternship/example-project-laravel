c8310982e4292fff477f1474fe3d08a285a10f50
gulverd <vakhtang.pataraia@gmail.com>
2019-12-25 17:23:43 +0400

    feat: Add User Roles table to Database

--- app/User.php
+++ app/User.php
@@ -81,4 +81,9 @@ class User extends Authenticatable implements JWTSubject
         return $this->belongsToMany(Charger::class, 'favorites', 'user_id', 'charger_id')->withTimeStamps();
     }
 
+    public function role()
+    {
+        return $this -> hasMany('App\Role');
+    }
+
 }
---
+++ database/migrations/2019_12_25_130547_create_roles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            [   
+                'name' => 'Regular',
+            ],
+            [
+                'name' => 'Admin'
+            ],
+            [
+                'name' => 'Business'
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
