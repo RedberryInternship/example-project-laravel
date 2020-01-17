e1235d36532aebe17cda0bb3f4586d6c01ade29b
gulverd <vakhtang.pataraia@gmail.com>
2020-01-17 14:32:55 +0400

    fix: georgian card gateway test

--- config/georgian-card-gateway.php
+++ config/georgian-card-gateway.php
@@ -3,6 +3,8 @@
 return [
 	'merchant_id' => env('MERCHANT_ID', ''),
 	'page_id' => env('PAGE_ID', ''),
-	'account_id_gel' => env('ACCOUNT_ID_GEL', ''),
+	'account_id' => env('ACCOUNT_ID', ''),
 	'ccy' => env('CCY', ''),
+	'back_url_f' => env('BACK_URL_S', ''),
+	'back_url_s' => env('BACK_URL_F', '')
 ];
