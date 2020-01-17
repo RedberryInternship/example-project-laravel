84804e4ffa33f7e3c608f06c92b3126ad489b0b1
gulverd <vakhtang.pataraia@gmail.com>
2020-01-17 13:04:38 +0400

    fix: send sms function

--- app/Http/Controllers/Api/app/V1/UserController.php
+++ app/Http/Controllers/Api/app/V1/UserController.php
@@ -60,6 +60,7 @@ class UserController extends Controller
         {
             $temp -> phone_number = $request -> get('phone_number');
             $temp -> code         = $rand;
+            $temp -> updated_at   = Carbon::now();
             $temp -> save();
         }else{
             $temp = TempSmsCode::create([
