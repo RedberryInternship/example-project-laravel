7f0a6a85add1746e321f31152c02cbeb7f7606a7
gulverd <vakhtang.pataraia@gmail.com>
2020-01-17 14:16:50 +0400

    fix: georgian card payment test file

--- packages/redberry/georgian-card-gateway/src/test.blade.php
+++ packages/redberry/georgian-card-gateway/src/test.blade.php
@@ -7,7 +7,9 @@
 	<form action="https://3dacq.georgiancard.ge/payment/start.wsm" method="GET">
 		<input type="text" name="merchant_id" placeholder="merchant_id" value="{{ config('georgian-card-gateway.merchant_id') }}">
 		<input type="text" name="page_id" placeholder="page_id" value="{{ config('georgian-card-gateway.page_id') }}">
-		<input type="text" name="account_id_gel" placeholder="account_id_gel" value="{{ config('georgian-card-gateway.account_id_gel') }}">
+		<input type="text" name="account_id" placeholder="account_id" value="{{ config('georgian-card-gateway.account_id') }}">
+		<input type="text" name="back_url_f" placeholder="back_url_f" value="{{ config('georgian-card-gateway.back_url_f') }}">
+		<input type="text" name="back_url_s" placeholder="back_url_s" value="{{ config('georgian-card-gateway.back_url_s') }}">
 		<input type="text" name="ccy" placeholder="ccy" value="{{ config('georgian-card-gateway.ccy') }}">
 		<input type="text" name="amount" placeholder="amount">
 		<input type="submit" name="submit">
