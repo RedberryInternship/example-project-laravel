ca9fa4446131d53763a8b04ec3d5c7e5850c180f
gulverd <vakhtang.pataraia@gmail.com>
2020-01-20 15:17:31 +0400

    test: test GC gateway 1

--- packages/redberry/georgian-card-gateway/src/test.blade.php
+++ packages/redberry/georgian-card-gateway/src/test.blade.php
@@ -10,8 +10,8 @@
 		<input type="text" name="merch_id" placeholder="merch_id" value="{{ config('georgian-card-gateway.merchant_id') }}">
 		<input type="text" name="page_id" placeholder="page_id" value="{{ config('georgian-card-gateway.page_id') }}">
 		<input type="text" name="account_id" placeholder="account_id" value="{{ config('georgian-card-gateway.account_id') }}">
-		<input type="text" name="back_url_f" placeholder="back_url_f" value="{{ config('georgian-card-gateway.back_url_f') }}">
-		<input type="text" name="back_url_s" placeholder="back_url_s" value="{{ config('georgian-card-gateway.back_url_s') }}">
+		<input type="text" name="back_url_f" placeholder="back_url_f" value="https://api-dev.e-space.ge/failed">
+		<input type="text" name="back_url_s" placeholder="back_url_s" value="https://api-dev.e-space.ge/succeed">
 		<input type="text" name="ccy" placeholder="ccy" value="{{ config('georgian-card-gateway.ccy') }}">
 		<input type="text" name="o.amount" placeholder="amount">
 		<input type="text" name="o.order_id" placeholder="order_id" value="test123">
