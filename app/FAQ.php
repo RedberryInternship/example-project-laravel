65a74a1e57303d3de2811335d6c0cf680770d51a
imedacf <imedacf@gmail.com>
2020-02-25 10:36:01 +0000

    feat: make faq fields translatable

--- app/FAQ.php
+++ app/FAQ.php
@@ -3,13 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class FAQ extends Model
 {
+    use HasTranslations;
+
     /**
      * Table name.
      */
     protected $table = 'faqs';
+
+    /**
+     * Translatable fields.
+     */
+    public $translatable = ['question', 'answer'];
 
     /**
      * Fillable fields.
