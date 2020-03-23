e8a1baad6a7492d4de7612c500ceeafbb561a80c
giunashvili <giorgi.official@yahoo.com>
2020-03-23 15:17:24 +0400

    Add: additional data field into transaction logging

--- app/Http/Controllers/Api/ChargerTransactions/V1/TransactionController.php
+++ app/Http/Controllers/Api/ChargerTransactions/V1/TransactionController.php
@@ -6,17 +6,24 @@ namespace App\Http\Controllers\Api\ChargerTransactions\V1;
 use App\Http\Controllers\Controller;
 use Log;
 
-class TransactionController extends Controller{
+class TransactionController extends Controller
+{
 
-  public function finish($transaction_id){
-    Log::channel('transaction_stop')->info($transaction_id);
+  public function finish($transaction_id)
+  {
+    Log::channel('transaction_stop')->info([
+      'transaction_id' => $transaction_id,
+      'additional_data' => request() -> all(),
+    ]);
   }
 
-  public function update($transaction_id, $value){
+  public function update($transaction_id, $value)
+  {
     Log::channel('transaction_update')->info(
       [
         'transaction_id' => $transaction_id, 
         'value' => $value,
+        'additional_data' => request() -> all(),
         ]
       );
   }
