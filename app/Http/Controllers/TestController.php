47f8f41339f3a5235cebefba39e30bdc6dd4a0dc
giunashvili <giorgi.official@yahoo.com>
2020-04-21 17:22:52 +0400

    Add: new routes for swagger in order to get to misha's back

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,8 +9,71 @@ use App\Facades\Simulator;
  {
 
 	
-    public function index()
-    {   
-      dd( "Nothingness" );
+    public function __invoke()
+    {
+      
+    }
+
+    public function all()
+    {
+      return response() -> json(
+        Charger::all(),
+      );
+    }
+
+    public function find( $charger_id )
+    {
+      return response() -> json(
+        Charger::find( $charger_id ),
+      );
+    } 
+
+    public function start( $charger_id, $connector_id )
+    {
+      return response() -> json(
+        Charger::start( $charger_id, $connector_id ),
+      );
+    }
+
+    public function stop( $charger_id, $transactionID )
+    {
+      return response() -> json(
+        Charger::stop( $charger_id, $transactionID ),
+      );
+    }
+
+    public function transactionInfo( $transactionID )
+    {
+      return response() -> json(
+        Charger::transactionInfo( $transactionID ),
+      );
+    }
+    
+    public function switchChargerIntoLvl2( $charger_id )
+    {
+      return response() -> json(
+        Simulator::activateSimulatorMode( $charger_id ),
+      );
+    }
+
+    public function bringChargerOnline( $charger_id )
+    {
+      return response() -> json(
+        Simulator::upAndRunning( $charger_id ),
+      );
+    }
+
+    public function plugOffChargerConnectorCable( $charger_id )
+    {
+      return response() -> json(
+        Simulator::plugOffCable( $charger_id ),
+      );
+    }
+
+    public function shutdown( $charger_id )
+    {
+      return response() -> json(
+        Simulator::shutdown( $charger_id ),
+      );
     }
  }
--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,21 @@ Route::group(['namespace' => 'Api\ChargerTransactions\V1', 'prefix' => 'chargers
     Route::get('update/{transaction_id}/{value}','TransactionController@update');
 });
 
+Route::group(['prefix' => 'chargers_back'], function(){
+    
+    Route::get('start-charging/{charger_id}/{connector_id}', 'TestController@start');
+    Route::get('stop-charging/{charger_id}/{transactionID}', 'TestController@stop');
+    Route::get('transaction-info/{transaction_id}',          'TestController@transactionInfo');
+    Route::get('find/{charger_id}',                          'TestController@find');
+    Route::get('all',                                        'TestController@all');
+
+    Route::get('charger/{charger_id}/switch-to-lvl2',        'TestController@switchChargerIntoLvl2');
+    Route::get('charger/{charger_id}/bring-online',          'TestController@bringChargerOnline');
+    Route::get('charger/{charger_id}/plug-off',              'TestController@plugOffChargerConnectorCable');
+    Route::get('charger/{charger_id}/shutdown',              'TestController@shutdown');
+
+});
+
 Route::get('/test-twilio', 'Api\app\V1\UserController@testTwilio');
 
-Route::get('test','TestController@index');
+Route::get('test','TestController');
