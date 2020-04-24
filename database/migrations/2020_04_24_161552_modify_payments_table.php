b76ff40b25e6fd73360b993c1592289df259879b
giunashvili <giorgi.official@yahoo.com>
2020-04-24 16:36:44 +0400

    Modify: payments

---
+++ database/migrations/2020_04_24_161552_modify_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema :: table( 'payments', function( Blueprint $table ){
+            $table -> dropColumn( 'active' );
+            $table -> dropColumn( 'date' );
+            $table -> dropColumn( 'status' );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema :: table( 'payments', function( Blueprint $table ){
+            $table -> boolean( 'active' ) -> nullable();
+            $table -> string ( 'date'   ) -> nullable();
+            $table -> boolean( 'status' ) -> nullable();
+        });
+    }
+}
