89b320920bd696c2cf173abccf1d4d1dad1e375b
imedacf <imedacf@gmail.com>
2020-05-05 18:24:31 +0400

    feat: add contact messages migration/model/controller/custom request/api service

---
+++ app/Http/Controllers/Api/app/V1/ContactMessageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\app\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactMessageRequest;
+
+class ContactMessageController extends Controller
+{
+    /**
+     * Store Contact Form Message in DB.
+     * 
+     * @param ContactMessageRequest $request
+     * 
+     * @return JSON
+     */
+    public function __invoke(ContactMessageRequest $request)
+    {
+        $request -> store();
+
+        return response() -> json([]);
+    }
+}
