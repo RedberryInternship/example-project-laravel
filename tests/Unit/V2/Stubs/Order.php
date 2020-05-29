2165f9a20c197ba239208ca4f63d12cc4e2b9ec6
giunashvili <giorgi.official@yahoo.com>
2020-05-29 14:21:45 +0000

    add: fast charging flow

--- tests/Unit/V2/Stubs/Order.php
+++ tests/Unit/V2/Stubs/Order.php
@@ -3,7 +3,11 @@
 namespace Tests\Unit\V2\Stubs;
 
 use Tests\Unit\V2\Stubs\ChargerConnectorType as CCTStub;
+use Tests\Unit\V2\Stubs\ChargingPrice as CPStub;
+use App\Enums\ChargingType as ChargingTypeEnum;
 use App\Enums\ChargerType as ChargerTypeEnum;
+use App\Enums\OrderStatus as OrderStatusEnum;
+use App\Enums\PaymentType as PaymentTypeEnum;
 use App\Order as OrderModel;
 use App\Facades\Simulator;
 use App\UserCard;
@@ -11,7 +15,7 @@ use App\Kilowatt;
 
 class Order
 {
-  public static function makeOrder( int $user_id, bool $isChargerFast = true )
+  public static function makeOrder( int $user_id, bool $isChargerFast = true , bool $isByAmount = false )
   {
 
     $userCard = factory( UserCard :: class ) -> create(
@@ -33,8 +37,17 @@ class Order
         'user_id'                   => $user_id,
         'charger_connector_type_id' => $chargerConnectorType -> id,
         'user_card_id'              => $userCard -> id,
+        'target_price'              => rand( 50, 100 ),
+        'charging_type'             => $isByAmount ? ChargingTypeEnum :: BY_AMOUNT : ChargingTypeEnum :: FULL_CHARGE,
       ]
     );
+
+    if( $chargerType == ChargerTypeEnum :: FAST )
+    {
+      $order -> updateChargingStatus( OrderStatusEnum :: CHARGING );
+      $order -> pay( PaymentTypeEnum :: CUT, 20 );
+      CPStub :: createFastChargingPrices( $chargerConnectorType -> id );
+    }
 
     factory( Kilowatt :: class ) -> create( 
       [
