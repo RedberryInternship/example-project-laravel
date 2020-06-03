a41039c4ba7221af5702693dc8e5c42132d06c85
giunashvili <giorgi.official@yahoo.com>
2020-06-03 13:32:24 +0000

    Add: additional methods to Transaction

--- packages/redberry/georgian-card-gateway/src/Transaction.php
+++ packages/redberry/georgian-card-gateway/src/Transaction.php
@@ -4,9 +4,23 @@ namespace Redberry\GeorgianCardGateway;
 
 class Transaction
 {
+  /**
+   * Georgian card base url.
+   * 
+   * @var string $url
+   */
   private $url;
+
+  /**
+   * Get request data which will be passed.
+   * 
+   * @var array $data
+   */
   private $data;
 
+  /**
+   * Set initial private parameters.
+   */
   public function __construct()
   {
     $this -> url = 'https://3dacq.georgiancard.ge/payment/start.wsm?';
@@ -23,27 +37,81 @@ class Transaction
     ];
   }
 
+  /**
+   * Build url and redirect to
+   * georgian card page.
+   * 
+   * @return mixed
+   */
   public function execute()
   {
     return redirect( $this -> buildUrl() );
   }
 
-  public function setAmount( float $amount )
+  /**
+   * Set amount in tetris.
+   * 100 = 1 GEL
+   * 
+   * @param   int $amount
+   * @return  void
+   */
+  public function setAmount( int $amount ): void
   {
     $this -> data [ 'o.amount' ] = $amount;
   }
 
-  public function setOrderId( string $orderId )
+  /**
+   * Set order id.
+   * 
+   * @param   string $orderId
+   * @return  void
+   */
+  public function setOrderId( string $orderId ): void
   {
     $this -> data [ 'o.id' ] = $orderId;
   }
 
-  public function enablePreauth()
+  /**
+   * Set user id.
+   * 
+   * @param  string $userId
+   * @return void
+   */
+  public function setUserId( string $userId ): void
+  {
+    $this -> data [ 'o.user_id' ] = $userId;
+  }
+
+  /**
+   * Set user card id.
+   * 
+   * @param   string $userCardId
+   * @return  void
+   */
+  public function setUserCardId( string $userCardId ): void
+  {
+    $this -> data [ 'o.user_card_id' ] = $userCardId;
+  }
+
+  /**
+   * Enable preauth.
+   * As of this moment I have no idea
+   * what this means.
+   * 
+   * @return  void
+   */
+  public function enablePreauth(): void
   {
     $this -> data [ 'preauth' ] = 'Y';
   }
 
-  public function passResultingData(array $data )
+  /**
+   * Pass additional data which will be present
+   * in the last step success or fail.
+   * 
+   * @return void
+   */
+  public function passResultingData(array $data ): void
   {
     $this -> data [ 'back_url_f' ] .= '?';
     $this -> data [ 'back_url_s' ] .= '?';
@@ -55,7 +123,12 @@ class Transaction
     }
   }
 
-  private function buildUrl()
+  /**
+   * Build get request url.
+   * 
+   * @return string
+   */
+  public function buildUrl()
   {
     $url = $this -> url;
 
