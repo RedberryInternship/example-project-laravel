dfa5a289d565ccd677f7946f86cb2d77be2b9324
giunashvili <giorgi.official@yahoo.com>
2020-06-04 11:26:56 +0000

    refactor: restructured

--- app/ChargerConnectorType.php
+++ app/ChargerConnectorType.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-use App\Entities\ChargerConnectorType as Entity;
+use App\Library\Entities\ChargerConnectorType as Entity;
 
 class ChargerConnectorType extends Model
 {
