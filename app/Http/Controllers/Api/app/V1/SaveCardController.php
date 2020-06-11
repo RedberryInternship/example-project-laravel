ff5a3675a50cf6d3204b9072a9ca26653b176b4c
giunashvili <giorgi.official@yahoo.com>
2020-06-11 11:33:44 +0000

    add: saved card controller success and failed urls

--- app/Http/Controllers/Api/app/V1/SaveCardController.php
+++ app/Http/Controllers/Api/app/V1/SaveCardController.php
@@ -10,11 +10,15 @@ class SaveCardController extends Controller
 {
   public function __invoke()
   {
-    $url = SaveCardInitiator :: getURL();
-    
+    $url        = SaveCardInitiator :: getURL();
+    $failedUrl  = config( 'georgian-card-gateway' )[ 'back_url_f' ];
+    $successUrl = config( 'georgian-card-gateway' )[ 'back_url_s' ];
+
     return response() -> json(
       [
         'save_card_url' => $url,
+        'success_url'   => $successUrl,
+        'failed_url'    => $failedUrl,
       ]
     );
   }
--- config/espace.php
+++ config/espace.php
@@ -4,12 +4,11 @@ return [
 
      /*
     |--------------------------------------------------------------------------
-    | Misha's Api Protocol and Ip
+    | Espace specific configs.
     |--------------------------------------------------------------------------
     */
 
     'mishas_back_protocol' => env('MISHAS_BACK_PROTOCOL', 'http'),
-
-    'mishas_back_ip' => env('MISHAS_BACK_IP', '13.92.63.164'),
+    'mishas_back_ip'       => env('MISHAS_BACK_IP', '13.92.63.164'),
 
 ];
