dee7bbfe937926b7d8b74e2010ec23dd1d7ce74f
imedacf <imedacf@gmail.com>
2020-06-24 10:29:23 +0400

    remove authentication debug code

--- app/Http/Controllers/Api/app/V1/User/AuthController.php
+++ app/Http/Controllers/Api/app/V1/User/AuthController.php
@@ -17,7 +17,6 @@ class AuthController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return response() -> json( $request -> all() );
         $credentials = $request -> only('phone_number', 'password');
 
         if ( ! $token = auth('api') -> attempt($credentials))
