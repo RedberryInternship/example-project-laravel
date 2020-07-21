222ab4abee9038b3f49d78e2006d5a73fca49eeb
giunashvili <giorgi.official@yahoo.com>
2020-07-21 11:10:18 +0000

    add: check orders if they're on penalty cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,10 +22,9 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule( Schedule $schedule )
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule -> command( 'command:check-orders-on-penalty' ) -> everyMinute();
     }
 
     /**
---
+++ app/Library/Interactors/CronJobs/OrdersOnPenaltyChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Library\Interactors\CronJobs;
+
+use App\Library\Entities\CronJobs\OrdersOnPenalty\OrdersGetter;
+use App\Library\Entities\CronJobs\OrdersOnPenalty\OrdersEditor;
+
+class OrdersOnPenaltyChecker
+{
+  /**
+   * Check orders if they are on penalty,
+   * if so then change the charging_status.
+   * 
+   * @return void
+   */
+  public static function check(): void
+  {
+    $orders = OrdersGetter :: get();
+    OrdersEditor :: update( $orders );
+    # scheduler every minute !
+  }
+}
