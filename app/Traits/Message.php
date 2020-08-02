ff90c23e2602c3efe5abc9d8b07e2c668575c0f6
giunashvili <giorgi.official@yahoo.com>
2020-08-02 19:37:17 +0000

    fix: message locale

--- app/Traits/Message.php
+++ app/Traits/Message.php
@@ -15,8 +15,8 @@ trait Message
   private $messages = [
     'something_went_wrong'                => [
       'en' => 'Something went wrong...',
-      'ka' => 'დაფიქსირდა შეცდომა...',
       'ru' => 'Что-то пошло не так...',
+      'ka' => 'დაფიქსირდა შეცდომა...',
     ],
     'charger_is_not_free'                 => [
       'en' => 'The Charger is not free.',
@@ -25,33 +25,33 @@ trait Message
     ],
     'charging_successfully_started'       => [
       'en' => 'Charging successfully started!',
-      'ka' => 'დამუხტვა წარმატებით დაიწყო!',
       'ru' => 'Зарядка успешно началась!',
+      'ka' => 'დამუხტვა წარმატებით დაიწყო!',
     ],
     'cant_charge'                         => [
       'en' => 'Charging couldn\'t be started.',
-      'ka' => 'დამუხტვის დაწყება ვერ ხერხდება.',
       'ru' => 'Зарядка не может быть начата.',
+      'ka' => 'დამუხტვის დაწყება ვერ ხერხდება.',
     ],
     'offline'                             => [
       'en' => 'Charger is offline!',
-      'ka' => 'დამტენი გამორთულია!',
       'ru' => 'Зарядное устройство не в сети!',
+      'ka' => 'დამტენი გამორთულია!',
     ],
     'cant_stop_charging'                  => [
       'en' => 'Stop charging request couldn\'t be confirmed.',
-      'ka' => 'დამუხტვის შეჩერების დადასტურება ვერ მოხერხდა.',
       'ru' => 'Запрос на прекращение зарядки не может быть подтвержден.',
+      'ka' => 'დამუხტვის შეჩერების დადასტურება ვერ მოხერხდა.',
     ],
     'charger_transaction_already_stopped' => [
       'en' => 'Charger ransaction is already finished!',
-      'ka' => 'Зарядное устройство уже завершено!',
-      'ru' => 'დატენვის ტრანზაქცია უკვე დასრულებულია.',
+      'ru' => 'Зарядное устройство уже завершено!',
+      'ka' => 'დატენვის ტრანზაქცია უკვე დასრულებულია.',
     ],
     'charging_successfully_finished'      => [
       'en' => 'Charging successfully finished!',
-      'ka' => 'დამუხტვა წარმატებით დასრულდა!',
       'ru' => 'Зарядка успешно остановлена!',
+      'ka' => 'დამუხტვა წარმატებით დასრულდა!',
     ],
   ];
 }
