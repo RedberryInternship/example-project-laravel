f8024ca9b8615f79de158748ec2e5b005228da7e
imedacf <imedacf@gmail.com>
2020-08-10 14:36:48 +0400

    feat: payments excel in nova

--- app/Nova/Order.php
+++ app/Nova/Order.php
@@ -4,11 +4,9 @@ namespace App\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
-use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Fields\DateTime;
 use App\Nova\Filters\OrderFinished;
 use App\Nova\Filters\OrderConfirmed;
@@ -17,6 +15,7 @@ use App\Nova\Filters\OrderRequestedAlready;
 use App\Nova\Filters\OrderStatus;
 use Titasgailius\SearchRelations\SearchesRelations;
 use Maatwebsite\LaravelNovaExcel\Actions\DownloadExcel;
+
 
 class Order extends Resource
 {
@@ -35,6 +34,17 @@ class Order extends Resource
      * @var string
      */
     public static $title = 'id';
+
+    /**
+     * Override Resource Title.
+     */
+    public function title()
+    {
+        return
+            $this -> user -> first_name . ' ' .
+            $this -> user -> last_name . ' - ' .
+            $this -> id;
+    }
 
     /**
      * The columns that should be searched.
