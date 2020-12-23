83cd193659639501493aac9d411dd968350ae051
giunashvili <giorgi.official@yahoo.com>
2020-12-23 09:21:30 +0400

    remove: whitelist hiding logic from back-end

--- app/Http/Controllers/Api/app/V1/ChargerController.php
+++ app/Http/Controllers/Api/app/V1/ChargerController.php
@@ -23,45 +23,7 @@ class ChargerController extends Controller
         $chargers = Charger :: OrderBy('id', 'desc')
                 -> where('status', '!=', ChargerStatusEnum :: NOT_PRESENT)
                 -> withAllAttributes()
-                -> get() 
-                -> filter(function( $charger ) use( $user ) {
-            
-                    /**
-                     * if charger is not hidden show it to the user.
-                     */
-                    if(! $charger -> hidden) 
-                    {
-                        return true;
-                    }
-        
-                    /**
-                     * If user is not authenticated and 
-                     * charger is hidden.
-                     */
-                    if(! $user )
-                    {
-                        return false;
-                    }
-        
-                    /**
-                     * If user is authenticated and charger is hidden
-                     * with user's phone_number in its whitelist, 
-                     * in that case show charger.
-                     */
-                    foreach( $charger -> whitelist as $allowedMember )
-                    {
-                        if($allowedMember -> phone === $user -> phone_number) 
-                        {
-                            return true;
-                        }
-                    }
-        
-                    /**
-                     * if user is authenticated and his/her phone number is not
-                     * in charger's whitelist, then hide charger from that user.
-                     */
-                    return false;
-                });
+                -> get();
 
         Charger::addChargingPrices($chargers);
         Charger::addIsFreeAttributes($chargers);
