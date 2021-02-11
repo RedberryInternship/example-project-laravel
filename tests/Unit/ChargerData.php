693578c9a57d5a592b6f79c3a2ebc26514ca7fef
giunashvili <giorgi.official@yahoo.com>
2021-02-11 18:30:46 +0400

    add: tests for used chargers

--- tests/Unit/ChargerData.php
+++ tests/Unit/ChargerData.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Charger;
+use App\ChargerConnectorType;
+use App\Order;
 use Tests\TestCase;
 
 class ChargerData extends TestCase 
@@ -11,27 +13,61 @@ class ChargerData extends TestCase
   {
     parent :: setUp();
     factory( Charger :: class, 3 ) -> create();
-    $this -> getChargersUrl = $this -> uri . 'chargers';
-    $this -> getChargerUrl = $this -> uri . 'charger/';
+    $this -> getChargersURL = $this -> uri . 'chargers';
+    $this -> getChargerURL = $this -> uri . 'charger/';
+    $this -> getUserChargersURL = $this -> uri . 'user-chargers';
+    $this -> user = $this -> createUser();
   }
 
   /** @test */
   public function get_chargers_gives_ok(): void
   {
-    $this -> get( $this -> getChargersUrl ) -> assertOk();
+    $this -> get( $this -> getChargersURL ) -> assertOk();
   }
 
   /** @test */
   public function get_charger_gives_ok(): void
   {
-    $this -> get( $this -> getChargerUrl . '1') -> assertOk();
-    $this -> get( $this -> getChargerUrl . '2') -> assertOk();
-    $this -> get( $this -> getChargerUrl . '3') -> assertOk();
+    $this -> get( $this -> getChargerURL . '1') -> assertOk();
+    $this -> get( $this -> getChargerURL . '2') -> assertOk();
+    $this -> get( $this -> getChargerURL . '3') -> assertOk();
   }
 
   /** @test */
   public function get_charger_gives_failure(): void
   {
-    $this -> get( $this -> getChargerUrl . '15213' ) -> assertStatus(404);
+    $this -> get( $this -> getChargerURL . '15213' ) -> assertStatus(404);
+  }
+
+  /** @test */
+  public function user_chargers_gives_ok(): void
+  {
+    factory( Charger :: class, 4 ) -> create()
+      -> each( function ( $charger ) {
+        $chargerConnectorType = factory( ChargerConnectorType :: class ) -> create(
+          [
+            'charger_id' => $charger -> id,
+          ]
+        );
+
+        factory( Order :: class ) -> create(
+          [
+            'charger_connector_type_id' => $chargerConnectorType -> id,
+            'user_id' => $this -> user -> id,
+          ]
+        );
+      });
+
+    $this 
+      -> actAs( $this -> user )
+      -> get( $this ->  getUserChargersURL )
+      -> assertOk()
+      -> assertJsonCount(3, 'data');
+    
+    $this 
+      -> actAs( $this -> user )
+      -> get( $this ->  getUserChargersURL . '/' . 4 )
+      -> assertOk()
+      -> assertJsonCount(4, 'data');
   }
 }
